b8de61eee9fce24d6ae122448e8f55b58e8058b6
lucas gelfond <lucasgelfond@gmail.com>
2026-01-11 11:12:40 -0500

    BE CORS config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,24 @@ import { pagesRoute } from "./routes/pages";
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.use("*", cors());
+// Tech debt - you'll need to add any URL we use for the frontend (i.e. if we swap domains) here
+app.use(
+	"*",
+	cors({
+		origin: (origin) => {
+			if (
+				origin === "https://searchable-whole-earth.pages.dev" ||
+				origin.startsWith("http://localhost")
+			) {
+				return origin;
+			}
+			return null;
+		},
+		allowHeaders: ["Content-Type"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		maxAge: 86400,
+	})
+);
 
 app.use("*", async (c, next) => {
 	return runWithEnv(c.env, () => next());
